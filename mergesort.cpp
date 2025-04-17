d97a9dd3d063d77eadc847a5fdb57277bf5b7158
iizaghz <sukmahawaiza@gmail.com>
2025-04-17 16:24:44 +0700

    add panjang element array

---
+++ mergesort.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int arr[20], B[20];
+
+int n;
+
+void input () {
+    while (true) {
+        cout << "Masukkan Panjang element array: " ;
+        cin >> n;
+
+    }
+}
